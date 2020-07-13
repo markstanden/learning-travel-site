ef776098efdbcba5c5a6c141eb9f8242f172bee2
Mark Standen <markstanden80@icloud.com>
2020-07-13 13:21:19 +0100

    more commenting and tweaking of base scss files

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   /*
     AUTOPREFIXER
+    
     add to package.json to configure behaviour:
     ( https://github.com/postcss/autoprefixer )
     ( https://github.com/browserslist/browserslist#queries )

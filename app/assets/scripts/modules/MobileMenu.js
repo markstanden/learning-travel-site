4251c027a08f8b66ff5de23e974f33f9d2c5c49b
Mark Standen <markstanden80@icloud.com>
2020-07-19 13:08:42 +0100

    header styling

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ class MobileMenu {
   constructor() {
     this.menuIcon = document.getElementsByClassName('site-header__menu-icon')[0];
     this.menuContent = document.getElementsByClassName('site-header__menu-content')[0];
+    this.siteHeader = document.getElementsByClassName('site-header')[0];
     this.events();
   }
 
@@ -11,6 +12,7 @@ class MobileMenu {
 
   toggleTheMenu() {
     this.menuContent.classList.toggle('site-header__menu-content--is-visible');
+    this.siteHeader.classList.toggle('site-header--is-expanded');
   }
 }
 
